5f4e61da2d7dab26f2542ffb8b7c5133af02fd57
Jernej Kavka (JK) <jernejkavka@ssw.com.au>
2019-09-08 22:31:12 +1000

    Initial commit.

---
+++ CognitiveServicesDemo.CustomerSupport/CognitiveServicesDemo.CustomerSupport/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CognitiveServicesDemo.CustomerSupport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
